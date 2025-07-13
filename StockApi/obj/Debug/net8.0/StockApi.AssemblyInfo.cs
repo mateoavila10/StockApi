c2f21187633ecad33144d5f936bfa41ec3fdc63d
Agustín <agustinberenguel1999@gmail.com>
2025-07-13 19:42:53 -0300

    Proveedores y soporte

--- StockApi/obj/Debug/net8.0/StockApi.AssemblyInfo.cs
+++ StockApi/obj/Debug/net8.0/StockApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dccd694459f5151231b56e74b835c02fbbe4302")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74cda715efb75201b6fa5c97a772fe885616332d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
